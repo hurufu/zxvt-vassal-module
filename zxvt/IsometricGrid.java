028d7835d1641631270d6df84f9a9c242b0fcf2d
Aleksy Grabowski <hurufu@gmail.com>
2020-05-24 10:27:55 +0200

    Commiting of some already present changes

--- zxvt/IsometricGrid.java
+++ zxvt/IsometricGrid.java
@@ -1,68 +1,25 @@
 package zxvt;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
 import zxvt.IsometricGridNumbering;
-
 import VASSAL.build.module.map.boardPicker.board.GeometricGrid;
 import VASSAL.build.module.map.boardPicker.board.mapgrid.GridNumbering;
-import VASSAL.build.module.documentation.HelpFile;
-import VASSAL.build.AbstractConfigurable;
-import VASSAL.build.Buildable;
-
 import java.awt.Point;
 import java.awt.geom.Area;
 
-public class IsometricGrid extends AbstractConfigurable implements GeometricGrid {
+public class IsometricGrid implements GeometricGrid {
+
+    private GridNumbering mNumbering;
 
     @Override
-    public Class<Integer>[] getAttributeTypes() {
-        return null;
-    }
-
-    @Override
-    public String[] getAttributeDescriptions() {
-        return null;
-    }
-
-    @Override
-    public String getAttributeValueString(String key) {
-        return "";
-    }
-
-    @Override
-    public void setAttribute(String key, Object value) {
-    }
-
-    @Override
-    public String[] getAttributeNames() {
-        return null;
-    }
-
-    @Override
-    public void addTo(Buildable parent) {
-    }
-
-    @Override
-    public Class[] getAllowableConfigureComponents() {
-        return null;
-    }
-
-    @Override
-    public HelpFile getHelpFile() {
-        return new HelpFile();
-    }
-
-    @Override
-    public void removeFrom(Buildable parent) {
-    }
-
-    @Override
-    public Area getGridShape(Point center, int range) {
+    public Area getGridShape(final Point center, final int range) {
         return new Area();
     }
 
     @Override
     public Point snapTo(java.awt.Point p) {
-        return new Point();
+        return p;
     }
 
     @Override
@@ -72,7 +29,7 @@ public class IsometricGrid extends AbstractConfigurable implements GeometricGrid
 
     @Override
     public String locationName(Point p) {
-        return "";
+        return mNumbering != null ? mNumbering.locationName(p) : null;
     }
 
     @Override
@@ -81,27 +38,34 @@ public class IsometricGrid extends AbstractConfigurable implements GeometricGrid
     }
 
     @Override
-    public Point getLocation(String location) throws BadCoords {
-        return new Point();
+    public Point getLocation(final String location) throws BadCoords {
+        if (mNumbering == null) {
+            throw new BadCoords("Can't get location if numbering isn't sepcified");
+        }
+        return mNumbering.getLocation(location);
     }
 
     @Override
     public int range(Point p1, Point p2) {
-        return 0;
+      return abs((int) floor((p2.x - p1.x) / 0.5))
+           + abs((int) floor((p2.y - p1.y) / 0.5));
     }
 
     @Override
     public boolean isVisible() {
-        return false;
+        return mNumbering != null ? mNumbering.isVisible() : null;
     }
 
     @Override
     public void draw(java.awt.Graphics g, java.awt.Rectangle bounds, java.awt.Rectangle visibleRect, double scale, boolean reversed) {
+        if (mNumbering != null) {
+            mNumbering.draw(g, bounds, visibleRect, scale, reversed);
+        }
     }
 
     @Override
     public GridNumbering getGridNumbering() {
-        return new IsometricGridNumbering();
+        return mNumbering;
     }
 
 }
